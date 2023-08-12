937f8853803f3bb58a7f92b6e1155c04cfae0bc2
abie_x <abhiramzmenon@gmail.com>
2023-08-12 15:53:01 +0530

    modifid the schema of blogs

--- data/blogs.js
+++ data/blogs.js
@@ -1,56 +1,141 @@
-const blogs = {
-    category: "Travel Tips",
-    title: "Exploring the World: Travel Guide",
-    titleImage: "travel-guide.jpg",
-    titleImageAltText: "Exploring the World: Travel Guide",
-    subSections: [
+// const blogs = {
+//     category: "Travel Tips",
+//     title: "Exploring the World: Travel Guide",
+//     Author: "Anupama Sreekumar",
+//     titleImage: "travel-guide.jpg",
+//     titleImageAltText: "Exploring the World: Travel Guide",
+//     blogContent: [
+//         {
+//             subTitle: "Packing Essentials",
+//             subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//             imagesSection:
+//             {
+//                 subImg1: "packing-essentials-img1.jpg",
+//                 subImgAltText1: "Packing Essentials Image 1",
+//                 subImg2: "packing-essentials-img2.jpg",
+//                 subImgAltText2: "Packing Essentials Image 2"
+//             }
+//         },
+//         {
+//             subTitle: "Navigating Local Cuisine",
+//             subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//             imagesSection:
+//             {
+//                 subImg1: "local-cuisine-img1.jpg",
+//                 subImgAltText1: "Navigating Local Cuisine Image 1",
+//                 subImg2: "local-cuisine-img2.jpg",
+//                 subImgAltText2: "Navigating Local Cuisine Image 2"
+//             }
+//         },
+//         {
+//             subTitle: "Hidden Gems to Explore",
+//             subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//             imagesSection:
+//             {
+//                 subImg1: "hidden-gems-img1.jpg",
+//                 subImgAltText1: "Hidden Gems Image 1",
+//                 subImg2: "hidden-gems-img2.jpg",
+//                 subImgAltText2: "Hidden Gems Image 2"
+//             }
+//         },
+//         {
+//             subTitle: "Budget-Friendly Travel Tips",
+//             subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//             imagesSection:
+//             {
+//                 subImg1: "budget-travel-img1.jpg",
+//                 subImgAltText1: "Budget-Friendly Travel Image 1",
+//             }
+//         }
+//     ]
+// }
+
+// export default blogs
+
+
+const blogs = [
+    {
+    title: "This is a test title",
+    category: "exploration",
+    author: "script",
+    blogContent: [
         {
-            subTitle: "Packing Essentials",
-            subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            imagesSection:
-            {
-                subImg1: "packing-essentials-img1.jpg",
-                subImgAltText1: "Packing Essentials Image 1",
-                subImg2: "packing-essentials-img2.jpg",
-                subImgAltText2: "Packing Essentials Image 2"
-            }
+            type: "h2",
+            content: "this is a sample h2 heading"
         },
         {
-            subTitle: "Navigating Local Cuisine",
-            subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            imagesSection:
-            {
-                subImg1: "local-cuisine-img1.jpg",
-                subImgAltText1: "Navigating Local Cuisine Image 1",
-                subImg2: "local-cuisine-img2.jpg",
-                subImgAltText2: "Navigating Local Cuisine Image 2"
-            }
+            type: "h3",
+            content: "this is a sample h2 heading"
         },
         {
-            subTitle: "Hidden Gems to Explore",
-            subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            imagesSection:
-            {
-                subImg1: "hidden-gems-img1.jpg",
-                subImgAltText1: "Hidden Gems Image 1",
-                subImg2: "hidden-gems-img2.jpg",
-                subImgAltText2: "Hidden Gems Image 2"
-            }
+            type: "h2",
+            content: "this is a second sample h2 heading"
         },
         {
-            subTitle: "Budget-Friendly Travel Tips",
-            subContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            imagesSection:
-            {
-                subImg1: "budget-travel-img1.jpg",
-                subImgAltText1: "Budget-Friendly Travel Image 1",
-                subImg2: "budget-travel-img2.jpg",
-                subImgAltText2: "Budget-Friendly Travel Image 2"
+            type: "p",
+            content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        },
+        {
+            type: "singleImg",
+            content: "this is a sample first image link"
+        },
+        {
+            type: "twoImg",
+            content: {
+                imgLink1: "sample img link",
+                img1AltText: "sample img alt text",
+                imgLink2: "sample img link 2",
+                img2AltText: "sample img link 2 alt text"
             }
-        }
+        },
     ]
-}
+    },
+    {
+        title: "This is a test title",
+        category: "exploration",
+        author: "script",
+        blogContent: [
+            {
+                type: "h2",
+                content: "this is a sample h2 heading"
+            },
+            {
+                type: "h3",
+                content: "this is a sample h2 heading"
+            },
+            {
+                type: "h2",
+                content: "this is a second sample h2 heading"
+            },
+            {
+                type: "p",
+                content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            },
+            {
+                type: "singleImg",
+                content: "this is a sample first image link"
+            },
+            {
+                type: "twoImg",
+                content: {
+                    imgLink1: "sample img link",
+                    img1AltText: "sample img alt text",
+                    imgLink2: "sample img link 2",
+                    img2AltText: "sample img link 2 alt text"
+                }
+            },
+            {
+                type: "twoImg",
+                content: {
+                    imgLink1: "sample img link",
+                    img1AltText: "sample img alt text",
+                    imgLink2: "sample img link 2",
+                    img2AltText: "sample img link 2 alt text"
+                }
+            },
+        ] 
+    }
+]
 
 export default blogs
-
 
--- model/blogModel.js
+++ model/blogModel.js
@@ -1,25 +1,81 @@
 import mongoose from "mongoose";
 
-const subImageSchema = new mongoose.Schema({
-    subImg1: String,
-    subImgAltText1: String,
-    subImg2: String,
-    subImgAltText2: String
-});
+// const subImageSchema = new mongoose.Schema({
+//     subImg1: String,
+//     subImgAltText1: String,
+//     subImg2: String,
+//     subImgAltText2: String
+// });
 
-const subSectionSchema = new mongoose.Schema({
-    subTitle: String,
-    subContent: String,
-    imagesSection: subImageSchema
-});
+// const subSectionSchema = new mongoose.Schema({
+//     subTitle: String,
+//     subContent: String,
+//     imagesSection: subImageSchema
+// });
 
-const blogSchema = new mongoose.Schema({
-    category: String,
+// const blogSchema = new mongoose.Schema({
+//     category: String,
+//     title: String,
+//     Author: String,
+//     titleImage: String,
+//     titleImageAltText: String,
+//     blogContent: [subSectionSchema]
+// });
+
+// Schema for a single image
+// const singleImageSchema = new mongoose.Schema({
+//     imgLink: String,
+//     altText: String
+//   });
+  
+//   // Schema for a two-image section
+//   const twoImageSchema = new mongoose.Schema({
+//     imgLink1: String,
+//     img1AltText: String,
+//     imgLink2: String,
+//     img2AltText: String
+//   });
+  
+//   // Schema for blog content section
+//   const blogContentSchema = new mongoose.Schema({
+//     h2: String,
+//     h3: String,
+//     p: String,
+//     singleImage: singleImageSchema,
+//     twoImage: twoImageSchema
+//   });
+  
+//   // Main blog schema
+//   const blogSchema = new mongoose.Schema({
+//     title: String,
+//     category: String,
+//     author: String,
+//     blogContent: [blogContentSchema]
+//   });
+
+const singleImageSchema = new mongoose.Schema({
+    imgLink: String,
+    altText: String
+  });
+  
+  const twoImageSchema = new mongoose.Schema({
+    imgLink1: String,
+    img1AltText: String,
+    imgLink2: String,
+    img2AltText: String
+  });
+  
+  const blogContentSchema = new mongoose.Schema({
+    type: String,
+    content: mongoose.Schema.Types.Mixed  // You can store different types of content
+  });
+  
+  const blogSchema = new mongoose.Schema({
     title: String,
-    titleImage: String,
-    titleImageAltText: String,
-    subSections: [subSectionSchema]
-});
+    category: String,
+    author: String,
+    blogContent: [blogContentSchema]
+  });
 
 const Blog = mongoose.model('Blog', blogSchema);
 
